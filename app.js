5c23cf168aa2a150dfe75c2aa073a1d515370bb1
kemalyusuke <kemalyusuke@gmail.com>
2021-02-18 21:33:36 +0700

    Done patch Socket

--- app.js
+++ app.js
@@ -12,9 +12,31 @@ const io = require('socket.io')(http, {
 });
 
 
-io.on('conection', (socket) => {
-  console.log('user connected')
+io.on('connection', (socket) => {
+  let id = socket.id
+  // console.log('user connected', socket)
+  // socket.broadcast.emit('userConnected', )
+  socket.on('connected', (data) => {
+    console.log(data)
+  })
+  
+  socket.on('newMessage', (data) => {
+    console.log(data, '<<<  masuk dari client');
+    socket.broadcast.emit("serverMessage", data)
+  })
+  socket.on('battle', (data) => {
+    let dice = Math.floor(Math.random() * 6) + 1
+    socket.emit('diceRoll', dice)
+    socket.broadcast.emit('enemyRoll', {id, dice})
+    
+  })
+  socket.on('newBattle', (data) => {
+    console.log('Masuk Juga');
+  })
+  // socket.emit('')
 })
+
+
 
 http.listen(PORT, () => {
   console.log('server running')
