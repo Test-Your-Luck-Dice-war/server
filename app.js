675c9c7d757adcb0c2741a122d4d811d8e15d8e9
robiul511 <robiultriawali@gmail.com>
2021-02-19 14:28:03 +0700

    perubahan untuk proses deploy server

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+if(process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const http = require('http').Server(app)
 const io = require('socket.io')(http, {
   cors: {
