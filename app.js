5248f62b6316cb6a115ea0604df83dfcb7763af9
kemalyusuke <kemalyusuke@gmail.com>
2021-02-19 10:01:50 +0700

    updated game mechanic on client

--- app.js
+++ app.js
@@ -12,32 +12,29 @@ const io = require('socket.io')(http, {
 });
 
 
-let name = []
+let room = []
 io.on('connection', (socket) => {
   let id = socket.id
-  // console.log(socket)
-  // console.log('user connected', socket)
-  // socket.broadcast.emit('userConnected', )
-  socket.on('disconnected', () => {
-    name = name.filter((el) => {
-      if(el.id !== id) {
-        return el
-      }
-    })
-    console.log(name)
-  })
-  socket.on('connected', (data) => {
-    if(name.length < 2) {
-      name.push({
+
+  socket.on('joinGame', (data) => {
+    if(room.length === 0) {
+      room.push({
         id,
         name: data.name
       })
+    } else if (room.length === 1) {
+      console.log(room);
+      room.push({
+        id,
+        name: data.name
+      })
+      let opponent = room[0]
       console.log('masuk if <<<<<<')
+      socket.emit('joinsBattle', opponent)
+      socket.broadcast.emit('joinsBattle', data)
     } else {
       socket.emit('isRoomFull', true)
     }
-    console.log(name.length)
-    console.log(name)
   })
   
   socket.on('newMessage', (data) => {
@@ -48,7 +45,24 @@ io.on('connection', (socket) => {
     let dice = Math.floor(Math.random() * 6) + 1
     socket.emit('diceRoll', dice)
     socket.broadcast.emit('enemyRoll', {id, dice})
-    
+  })
+
+  socket.on('disconnected', () => {
+    console.log('masuk', '<<<<<<<<<<<<<<<');
+    room = []
+    socket.broadcast.emit('enemyLeft')
+    // console.log(room)
+  })
+
+  socket.on('disconnect', (val) => {
+    console.log(val);
+    room = room.filter((el) => {
+      if(el.id !== id) {
+        return el
+      }
+    })
+    socket.broadcast.emit('enemyLeft')
+    // console.log(room)
   })
   // socket.on('newBattle', (data) => {
   //   console.log('Masuk Juga');
